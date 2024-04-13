f2f7af022083c5b8de5d1aacadebf7355bfb5596
millenialprophet007 <166893817+millenialprophet007@users.noreply.github.com>
2024-04-13 12:16:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "local_file" "test" {
   filename = "${path.module}/shihabtestfile.txt"
-  content = "This is test file for local file module"
+  content = "This is test file for Jenkins job to run"
 }
